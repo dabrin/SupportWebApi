92568ac5d9d11013c18b44c572d8d67de90a2a13
Dabrin <dabrinltecr@gmail.com>
2020-02-20 16:19:08 -0600

    Detalles modificados

--- appAngularSupport/src/app/components/components/support/support.component.ts
+++ appAngularSupport/src/app/components/components/support/support.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Support } from '../models/Support';
+import { SupportService } from '../services/Support.service';
 //JALAR EL SERVICIO
 
 @Component({
@@ -11,15 +13,40 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SupportComponent implements OnInit {
 
     supportForm: FormGroup;
-    title: string = "Create";
-    id: number;
-    errorMessage: any;
+    Pass = new FormControl('', [Validators.required]);
+    Name = new FormControl('', [Validators.required]);
+    First_SurName = new FormControl('', [Validators.required]);
+    Second_Surname = new FormControl('', [Validators.required]);
+    Email = new FormControl('', [Validators.required]);
 
-    constructor() { }
+    constructor(private supportService: SupportService, public formBuilder: FormBuilder
+        ,
+        private router: Router) { }
 
     ngOnInit(): void {
 
     }
+
+    createSupport() {
+
+        const Pass = (document.querySelector('#Pass') as HTMLInputElement).value
+        const Name = (document.querySelector('#Name') as HTMLInputElement).value
+        const First_SurName = (document.querySelector('#First_L') as HTMLInputElement).value
+        const Second_Surname = (document.querySelector('#Second_L') as HTMLInputElement).value
+        const Email = (document.querySelector('#Email') as HTMLInputElement).value
+
+        this.supportForm = this.formBuilder.group({
+            Pass: Pass,
+            Name: Name,
+            First_SurName: First_SurName,
+            Second_Surname: Second_Surname,
+            Email: Email
+
+        })
+        this.supportService.createSupport(this.supportForm.value).subscribe(() => 'Succces', error => 'Error')
+
+    }
+
 
 
 }
