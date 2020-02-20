47dfa38af917b4a2716a64f829aa6bf74f2c1467
Pablo Rojas <pablo.rojas96@hotmail.com>
2020-02-20 15:03:07 -0600

    Eliminado

--- borrador/appAngularSupport/src/app/components/components/models/Issue.ts
+++
@@ -1,9 +0,0 @@
-export class Issue {
-    Report_Number: number;
-    Id_Supporter: number;
-    Classification: string;
-    Status: string;
-    Report_Time: string;
-    Resolution_Comment: string;
-
-}
