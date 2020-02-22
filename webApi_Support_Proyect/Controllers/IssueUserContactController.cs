bac02e5cc297fef1f8ddb8053fa5c13a8fe9cc71
Dabrin <dabrinltecr@gmail.com>
2020-02-22 12:01:45 -0600

    Creando clases para consumir Api Rest (springboot)

---
+++ webApi_Support_Proyect/Controllers/IssueUserContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+using webApi_Support_Proyect.Models;
+
+namespace webApi_Support_Proyect.Controllers
+{
+    public class IssueUserContactController : Controller
+    {
+        // GET: IssueUserContactModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //En teoría así se consume un Api Rest desde aquí
+        public IHttpActionResult GetById(int id)
+        {
+
+            string url= "http://localhost:8080/api/issue/";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+
+            IssueModel issue = null;
+
+            foreach (var i in m) {
+                issue.Report_Number = i.Report_Number;
+
+            }
+            
+
+
+            return Json(m);
+
+        }
+    }
+}
