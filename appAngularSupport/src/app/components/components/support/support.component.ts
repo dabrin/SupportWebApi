5c1e0ea5c8a39ac56c5eae56cc389d441ba83352
Pablo Rojas <pablo.rojas96@hotmail.com>
2020-02-28 00:20:34 -0600

    Validaciones soportista

--- appAngularSupport/src/app/components/components/support/support.component.ts
+++ appAngularSupport/src/app/components/components/support/support.component.ts
@@ -3,6 +3,8 @@ import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angula
 import { Router, ActivatedRoute } from '@angular/router';
 import { Support } from '../models/Support';
 import { SupportService } from '../services/Support.service';
+import swal from "sweetalert2";
+
 //JALAR EL SERVICIO
 
 @Component({
@@ -12,7 +14,7 @@ import { SupportService } from '../services/Support.service';
 })
 export class SupportComponent implements OnInit {
 
-
+/*
     support: boolean;
     supervisor: boolean;
     supportForm: FormGroup;
@@ -21,15 +23,73 @@ export class SupportComponent implements OnInit {
     First_SurName = new FormControl('', [Validators.required]);
     Second_Surname = new FormControl('', [Validators.required]);
     Email = new FormControl('', [Validators.required]);
+    */
 
-    constructor(private supportService: SupportService, public formBuilder: FormBuilder
-        ,
-        private router: Router) { }
+   form: FormGroup;
+   submitted = false;
+   error = '';
+   loading: boolean = false;
+
+  
+        constructor(private formBuilder: FormBuilder,
+            private router: Router,
+            private supportService: SupportService) {
+  this.form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    pass: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]],
+    First_L: ['', [Validators.required, Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]],
+    Second_L: ['', [Validators.required, Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]],
+ });
+}
+
 
     ngOnInit(): void {
 
     }
 
+    
+  submit() {
+    this.error = '';
+    this.submitted = true;
+    if (this.form.invalid || this.loading) return;
+    this.blockForm();
+    let user = new Support();
+    user.Email = this.email.value;
+    user.Pass = this.pass.value;
+    user.Name = this.name.value;
+    user.First_SurName = this.First_L.value;
+    user.Second_Surname = this.Second_L.value;
+    this.supportService.createSupport(user).subscribe(data => {
+      swal.fire({
+        icon: 'success',
+        text: 'El registro fue éxitoso'
+      }).finally(() => {
+        this.router.navigate(['/login'])
+      });
+    }, res => {
+      this.error = res.error.text;
+      this.unBlockForm();
+    })
+  }
+
+  blockForm() {
+    this.loading = true;
+    this.form.disable();
+  }
+
+  unBlockForm() {
+    this.loading = false;
+    this.form.enable();
+  }
+
+  get email() { return this.form.get('email'); }
+  get pass() { return this.form.get('pass'); }
+  get name() { return this.form.get('name'); }
+  get First_L() { return this.form.get('First_L'); }
+  get Second_L() { return this.form.get('Second_L'); }
+
+/*
     createSupport() {
 
         const Pass = (document.querySelector('#Pass') as HTMLInputElement).value
@@ -56,14 +116,8 @@ export class SupportComponent implements OnInit {
 
         console.log('Easy')
     }
-    foroMostrar() {
-
-        if (this.supervisor == true) {
-            console.log('superviso');
-
-        } else
-            console.log('No mames');
-    }
+    */
+ 
 
 
 
